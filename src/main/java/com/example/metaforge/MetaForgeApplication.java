fea14ef1b3d2e10ded07dd9ab71b358f2a8a46ef
Lizurt <lizard.x@mail.ru>
2024-12-23 14:34:14 +0300

    init

---
+++ src/main/java/com/example/metaforge/MetaForgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.metaforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/metaforge/MetaForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metaforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
